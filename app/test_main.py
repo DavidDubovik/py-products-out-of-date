100c65364701d02a0ea86728692e55688cc7e3ba
salodoxera <DavidDubovik19@gmail.com>
2024-11-10 14:07:57 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,61 +1,33 @@
 import datetime
+from unittest.mock import patch, MagicMock
+
 import pytest
-from unittest.mock import patch
-from freezegun import freeze_time
+
 from app.main import outdated_products
 
 
-@pytest.mark.parametrize(
-    "products, expected", [
-        (
-            [
-                {
-                    "name": "salmon",
-                    "expiration_date": datetime.date(2022, 2, 10),
-                    "price": 600,
-                },
-                {
-                    "name": "chicken",
-                    "expiration_date": datetime.date(2022, 2, 5),
-                    "price": 120,
-                },
-                {
-                    "name": "duck",
-                    "expiration_date": datetime.date(2022, 2, 1),
-                    "price": 160,
-                },
-            ],
-            ["duck"],
-        ),
-        ([], []),
-        (
-            [
-                {
-                    "name": "salmon",
-                    "expiration_date": datetime.date(2022, 2, 2),
-                    "price": 600,
-                },
-                {
-                    "name": "chicken",
-                    "expiration_date": datetime.date(2022, 1, 5),
-                    "price": 120,
-                },
-                {
-                    "name": "duck",
-                    "expiration_date": datetime.date(2022, 2, 1),
-                    "price": 160,
-                },
-            ],
-            ["chicken", "duck"],
-        ),
+@pytest.fixture()
+def products() -> list:
+    return [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
     ]
-)
-@freeze_time("2022-02-02")
-@patch("datetime.date.today")
-def test_outdated_products(
-    mock_today: patch, products: list[dict], expected: list[str]
-) -> None:
-    mock_today.return_value = datetime.date(2022, 2, 2)
 
-    result = outdated_products(products)
-    assert result == expected
+
+@patch("app.main.datetime")
+def test_outdated_products(mock_date: MagicMock, products: list) -> None:
+    mock_date.date.today.return_value = datetime.date(2022, 2, 2)
+    assert outdated_products(products) == ["duck"]
